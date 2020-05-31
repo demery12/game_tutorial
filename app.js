78e69ead22bc77f8b72968585ce1f559d58239b4
Dylan Emery <dylane884@gmail.com>
2020-05-31 16:19:50 -0400

    Project after third video in tutorial: multiple players can now connect, which is shown on a canvas that everyone can see

--- app.js
+++ app.js
@@ -10,14 +10,38 @@ app.use('/client', express.static(__dirname + '/client'));
 server.listen(2000);
 console.log("Server started.");
 
+const SOCKET_LIST = {};
+
 const io = require('socket.io')(server, {});
 io.sockets.on('connection', function(socket){
-    console.log('socket connection');
-    socket.on('happy', function(data){
-        console.log('happy because: ' + data.reason);
-    });
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
+    socket.number = "" + Math.floor(10 * Math.random());
+    SOCKET_LIST[socket.id] = socket;
 
-    socket.emit('serverMsg', {
-        msg:'hello',
-    });
+    console.log('socket connection', socket.id);
+
+    socket.on('disconnect', function(){
+        delete SOCKET_LIST[socket.id];
+    })
 });
+
+setInterval(function() {
+    const pack = [];
+    for(let i in SOCKET_LIST){
+        let socket = SOCKET_LIST[i]
+        socket.x++;
+        socket.y++;
+        pack.push({
+            x:socket.x,
+            y:socket.y,
+            number:socket.number
+        });
+    }
+    for(let i in SOCKET_LIST){
+        let socket = SOCKET_LIST[i]
+        socket.emit('newPosition', pack);
+    }
+
+}, 1000/25);
